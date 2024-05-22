b2990973dd3e56be7311f2fc4791391285bfe3f0
tudyt <tudytm@gmail.com>
2024-05-22 14:15:11 +0200

    Repair the hay block

--- src/main/java/net/tudytm/AdditionalFoodsMod/block/ModBlocks.java
+++ src/main/java/net/tudytm/AdditionalFoodsMod/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HayBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -21,7 +22,7 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, AdditionalFoodsMod.MOD_ID);
 
     public static final RegistryObject<Block> RICE_BLOCK = registerBlock("rice_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.HAY_BLOCK)));
+            () -> new HayBlock(BlockBehaviour.Properties.copy(Blocks.HAY_BLOCK)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
@@ -36,5 +37,4 @@ public class ModBlocks {
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
     }
-
 }
